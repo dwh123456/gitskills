7bf5e2cfb91ec3175eb1576fce94d480ecd1a8b3
unknown <1733512710@qq.com>
2019-02-02 14:42:23 +0800

    提交轮播图

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import App from './App'
 import router from './router'
 import fastClick from 'fastclick'
@@ -9,10 +10,12 @@ import './assets/styles/reset.css'
 //边框放大缩小都为相同的像素
 import './assets/styles/border.css' 
 import './assets/styles/iconfont.css'
+import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
 //去除click单击事件中默认的300ms
-fastClick.attach(document.body) 
+fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="wrapper">
+        <swiper :options="swiperOption" >
+            <!-- slides  ref="mySwiper" @someSwiperEvent="callback"-->
+            <swiper-slide v-for='item of swiperList' :key='item.id'>
+                <img class="swiper-img" :src="item.imgUrl" alt="">
+            </swiper-slide>
+            <!-- Optional controls -->
+            <div class="swiper-pagination"  slot="pagination"></div>
+        </swiper>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HomeSwiper',
+    data () {
+        return {
+            swiperOption: {
+                pagination: '.swiper-pagination',
+                loop: true //让轮播插件支持循环轮播
+            },swiperList: [{
+                id: '0001',
+                imgUrl: '//dimg04.c-ctrip.com/images/700p11000000q8vvaF87A_1242_470_191.jpg'
+            },{
+                id: '0002',
+                imgUrl: '//dimg04.c-ctrip.com/images/700510000000q9gcn0CD4_1242_470_191.jpg'
+            }]
+        }
+    }
+}
+</script>
+
+<style lang='stylus' scoped>
+    .wrapper >>> .swiper-pagination-bullet-active //进行样式穿透（当wrapper下面出现swiper-pagination-bullet-active就让他们都变成下面的样式）
+            background-color: #fff !important 
+    .wrapper
+        overflow hidden
+        width: 100%
+        height: 0
+        padding-bottom: 37.84%
+        background-color: #ccc
+        .swiper-img
+            width: 100%
+        //.swiper-pagination
+            
+</style>
+
+
