bd627ad7531bdf8f405ebae426e93666a5c42fb1
unknown <1733512710@qq.com>
2019-02-06 22:25:37 +0800

    提交使用了keep-alive标签的home组件

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <div id="app"> 
-    <router-view/>
+  <div id="app">
+    <keep-alive><!-- 被该标签包裹的，如果要加载的内容请求数据在本地，就直接在本地提取数据，如果没就向服务器请求 -->
+      <router-view/>
+    </keep-alive>
   </div>
 </template>
 
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -14,6 +14,7 @@ import HomeSwiper from './components/Swiper'
 import HomeIcons from './components/Icons'
 import HomeRecommend from './components/Recommend'
 import HomeWeekend from './components/Weekend'
+import { mapState } from 'vuex'
 
 import axios from 'axios'
 export default {
@@ -27,18 +28,22 @@ export default {
   },
   data () {
       return {
+        lastCity,
         swiperList: [],
         iconList: [],
         recommendList: [],
         weekendList: []
       }
   },
+  computed: {
+    ...mapState(['city'])
+  },
   methods: {
     getHomeInfo () {
       /*在用静态文件夹static时，有时要模拟后台数据，
       但又不想在在项目上线前来改动ajax的请求地址，
       可以在config/index.js中的proxyTable对象配置项中添加代理*/
-      axios.get('/api/index.json') 
+      axios.get('/api/index.json?city=' + this.city) 
         .then(this.getHomeInfoSucc)
     },
     getHomeInfoSucc (res) {
@@ -54,7 +59,14 @@ export default {
     }
   },
   mounted () {
+    this.lastCity = this.city
     this.getHomeInfo()
+  },
+  activated () { //activated生命周期函数 当页面重新被显示时，才执行
+    if (this.lastCity !== this.city) {
+      this.lastCity = this.city
+      this.getHomeInfo()
+    }
   }
 }
 </script>
